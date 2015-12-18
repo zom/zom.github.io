27ef8944e87addb1e4aff230a5bccc71b9b6d2c7
n8fr8 <nathan@freitas.net>
2015-12-17 23:09:45 -0500

    update message for wechat

--- javascript/wechat.js
+++ javascript/wechat.js
@@ -1,5 +1,5 @@
 
 if(navigator.userAgent.match(/MicroMessenger/i))
     {
-	alert("You are using WeChat! Please open this link in the external browser using the menu option.");
+	alert("You are using WeChat!\n\nIf you have Zom installed, please select menu option 'Open in Browser', then choose Zom.\n\nIf you do NOT have Zom installed, please select menu option 'Open in Browser', then choose an external web browser app");
     }
