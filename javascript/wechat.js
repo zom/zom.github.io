173e7f5b605984ab35c4f3ee1fb78e2708cdd20f
n8fr8 <nathan@freitas.net>
2016-06-22 11:52:09 -0400

    fix wechat

--- javascript/wechat.js
+++ javascript/wechat.js
@@ -1,5 +1,9 @@
 
-if(navigator.userAgent.match(/MicroMessenger/i))
+if (isWeixinBrowser())
     {
 	alert("You are using WeChat!\n\nIf you have Zom installed, please select menu option 'Open in Browser', then choose Zom.\n\nIf you do NOT have Zom installed, please select menu option 'Open in Browser', then choose an external web browser app");
     }
+
+function isWeixinBrowser(){
+  return /micromessenger/.test(navigator.userAgent.toLowerCase())
+}
